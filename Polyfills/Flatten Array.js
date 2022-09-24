0efc68c5fd41f23d58869709b4a1a921d9081b1a
Tanya Singh <52325969+tanyas27@users.noreply.github.com>
2022-09-24 15:31:14 +0530

    Create Flatten Array.js

---
+++ Polyfills/Flatten Array.js
@@ -0,0 +1,17 @@
+let inputArray = [1, 2, [3, 4], 5, [[[6, 7], 8, [[[[9]]]]]]];
+
+Array.prototype.flatten = function (){
+    let arr = this;
+    let res = [];
+    arr.forEach(item => {
+        debugger;
+        if(Array.isArray(item)){
+           res.push(...item.flatten());
+        } else {
+            res.push(item);
+        }
+    });
+    return res;
+}
+
+console.log(inputArray.flatten()); // [1, 2, 3, 4, 5, 6, 7, 8, 9]
